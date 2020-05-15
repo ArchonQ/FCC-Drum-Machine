aac17d5fc46befa9e3c2884cf8227aaad9bef1b9
brandon quezada <brandon.cypaso@gmail.com>
2020-05-15 10:43:36 -0500

    got sounds working onClick

--- fcc-drum-machine/src/components/box.js
+++ fcc-drum-machine/src/components/box.js
@@ -1,8 +1,23 @@
 import React from 'react';
 
 class Box extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.audio = React.createRef();
+	}
+
+	playSound = () => {
+		this.audio.current.play();
+	};
 	render() {
-		return <div className='box'>{this.props.text}</div>;
+		const { text, audio } = this.props;
+		return (
+			<div className='box' onClick={this.playSound}>
+				{text}
+				<audio ref={this.audio} src={audio} className='clip' id={text} />
+			</div>
+		);
 	}
 }
 
